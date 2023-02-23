62cc5dcdd1bdc487e77292119ef6d918c2b0a214
Aravindx <aravindmathi517@gmail.com>
2023-02-23 19:35:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './Header';
+import Cart from './Cart'; 
+function App() {
+  return (
+    <div className="App">
+         <Header/>
+        <Cart/>
+    </div>
+  );
+}
+
+export default App;
